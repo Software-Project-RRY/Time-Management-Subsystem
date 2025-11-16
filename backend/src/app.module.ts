5ffdfabe51c5ad482870fef8793aa900eb618daa
MadanyCode <ibrahimmadany5@gmail.com>
2025-11-16 16:04:06 +0200

    clock in clock out overtime exceptions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AttendanceModule } from './modules/attendance/attendance.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/time-management', ),
+    AttendanceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
