f6bd067cab663eff68e02f632618e68d8ff4448b
m4alek-0 <malek.khaled2104@gmail.com>
2025-11-16 20:57:56 +0200

    Exceptions & availability

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ExceptionsModule } from './exceptions/exceptions.module';
+import { AvailabilityModule } from './availability/availability.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    ExceptionsModule,
+    AvailabilityModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
