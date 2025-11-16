ae345e1df6e0219c54e2b52a60cb5602e216d15f
Yassin-Kassem <yassinkassem29@gmail.com>
2025-11-16 16:00:36 +0200

    Shifts And Assignments Modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ShiftModule } from './modules/shift/shift.module';
+import { AssignmentModule } from './modules/assignment/assignment.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    ShiftModule,
+    AssignmentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
